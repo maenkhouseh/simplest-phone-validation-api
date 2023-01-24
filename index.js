b9b7a466a70b7a42e588715fa7f7cb378d6b130f
Dr. Maen K Househ <maen.househ@evotik.com>
2023-01-25 02:02:22 +0300

    adding doc

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const express = require("express");
-const PhoneNumber = require("google-libphonenumber").PhoneNumber;
+const express = require("express"); // Import express module
 const phoneUtil =
-	require("google-libphonenumber").PhoneNumberUtil.getInstance();
-const app = express();
+  require("google-libphonenumber").PhoneNumberUtil.getInstance(); // Import Phone number utility from google-libphonenumber
+const app = express(); // Create an express application
 
-app.use(express.json());
+app.use(express.json()); // Use express middleware to parse JSON body
 
+/**
+ * POST /validate-phone endpoint
+ * Validate a phone number
+ *
+ * @param {string} countryCode - The country code of the phone number
+ * @param {string} phoneNumber - The phone number to validate
+ * @return {Object} - JSON object with validation result
+ */
 app.post("/validate-phone", (req, res) => {
-	const { countryCode, phoneNumber } = req.body;
+  const { countryCode, phoneNumber } = req.body; // Get country code and phone number from request body
 
-	if (!countryCode || !phoneNumber) {
-		return res
-			.status(400)
-			.json({ error: "Country code and phone number are required" });
-	}
+  // Validate presence of country code and phone number
+  if (!countryCode || !phoneNumber) {
+    return res
+      .status(400)
+      .json({ error: "Country code and phone number are required" });
+  }
 
-	const phoneNumberWithCountryCode = `+${countryCode}${phoneNumber}`;
+  const phoneNumberWithCountryCode = `+${countryCode}${phoneNumber}`; // Concatenate country code and phone number
 
-	try {
-		const phoneNumberObject = phoneUtil.parse(phoneNumberWithCountryCode, "");
-		if (!phoneUtil.isValidNumber(phoneNumberObject)) {
-			return res.status(400).json({ error: "Invalid phone number" });
-		}
-	} catch (err) {
-		return res.status(400).json({ error: "Invalid phone number" });
-	}
-	return res.json({ valid: true });
+  try {
+    const phoneNumberObject = phoneUtil.parse(phoneNumberWithCountryCode, ""); // parse the phone number
+    if (!phoneUtil.isValidNumber(phoneNumberObject)) {
+      return res.status(400).json({ error: "Invalid phone number" }); // If invalid phone number
+    }
+  } catch (err) {
+    return res.status(400).json({ error: "Invalid phone number" }); // If phone number is not valid
+  }
+  return res.json({ valid: true }); // If valid phone number
 });
 
-app.listen(1988, () => {
-	console.log("Phone number validation API listening on port 1988!");
+app.listen(3000, () => {
+  console.log("Phone number validation API listening on port 3000!");
 });
